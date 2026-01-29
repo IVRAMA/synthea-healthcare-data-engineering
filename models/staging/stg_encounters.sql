de9154c87c02474c11538e270af0db9115150c0e
rama chandra <iv.ramachandra@gmail.com>
2026-01-29 19:48:16 +0000

    created the csv data sources

---
+++ models/staging/stg_encounters.sql
@@ -0,0 +1,24 @@
+{{ config(materialized='view') }}
+
+WITH raw_encounters AS (
+  SELECT * 
+  FROM {{ source('clinical_data', 'encounters') }}
+)
+
+SELECT 
+  "ID",
+  TO_TIMESTAMP_NTZ("START")     AS "START_TIME",
+  TO_TIMESTAMP_NTZ("STOP")      AS "STOP_TIME",
+  "PATIENT",
+  "ORGANIZATION",
+  "PROVIDER",
+  "PAYER",
+  "ENCOUNTERCLASS",
+  TO_NUMBER("CODE")             AS "CODE",
+  "DESCRIPTION",
+  TRY_TO_DOUBLE("BASE_ENCOUNTER_COST")  AS "BASE_ENCOUNTER_COST",
+  TRY_TO_DOUBLE("TOTAL_CLAIM_COST")     AS "TOTAL_CLAIM_COST",
+  TRY_TO_DOUBLE("PAYER_COVERAGE")       AS "PAYER_COVERAGE",
+  "REASONCODE",
+  "REASONDESCRIPTION"
+FROM raw_encounters
