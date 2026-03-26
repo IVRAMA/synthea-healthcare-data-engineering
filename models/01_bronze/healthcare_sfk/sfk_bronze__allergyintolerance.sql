4b78c37e7d10a90e97292461a8540a24917dd06f
IVRAMA <40890530+IVRAMA@users.noreply.github.com>
2026-03-26 11:57:25 +0530

    table name changes in bronze schema

--- models/01_bronze/healthcare_sfk/sfk_bronze__allergyintolerance.sql
+++ models/01_bronze/healthcare_sfk/sfk_bronze__allergyintolerance.sql
@@ -4,14 +4,6 @@ source as (
 
     select * from {{ source('json_data', 'AllergyIntolerance') }}
 
-),
-
-renamed as (
-
-    select *
-
-    from source
-
 )
 
-select * from renamed
+select * from source
